b65f2a9304538b873ed8efe16c845c320f7f9b39
Crimson-Squadron <mcindo138@gmail.com>
2024-04-27 18:13:17 +0700

    Add Responsive Navbar

--- script.js
+++ script.js
@@ -15,8 +15,25 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     })
   }, bgOptions)
-  
   bgObserver.observe(banner)
+
+  var mobileNav = document.querySelector('.mobile-navbar-container')
+  var mNavBtn = document.querySelector('.mobile-nav-btn')
+  var mNavBtnIcon = mNavBtn.querySelector('.mobile-nav-btn img')
+
+  mNavBtn.addEventListener('click', function() {
+    const visibility = mobileNav.getAttribute('data-visible')
+
+    if (visibility == 'false') {
+      mobileNav.setAttribute('data-visible', true)
+      mNavBtn.setAttribute('aria-expanded', true)
+      mNavBtnIcon.src = 'images/logo/Close.svg'
+    } else {
+      mobileNav.setAttribute('data-visible', false)
+      mNavBtn.setAttribute('aria-expanded', false)
+      mNavBtnIcon.src = 'images/logo/Menu.svg'
+    }
+  })
 })
 
 
