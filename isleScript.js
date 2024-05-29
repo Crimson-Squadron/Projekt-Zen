7170d01f85d0240ad619944d76a725680e2a53f3
Crimson-Squadron <mcindo138@gmail.com>
2024-05-29 15:31:58 +0700

    Finish All Page Design

--- isleScript.js
+++ isleScript.js
@@ -1,3 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var mobileNav = document.querySelector('.mobile-navbar-container')
+    var mNavBtn = document.querySelector('.mobile-nav-btn')
+    var mNavBtnIcon = mNavBtn.querySelector('.mobile-nav-btn img')
+  
+    mNavBtn.addEventListener('click', function() {
+      const visibility = mobileNav.getAttribute('data-visible')
+  
+      if (visibility == 'false') {
+        mobileNav.setAttribute('data-visible', true)
+        mNavBtn.setAttribute('aria-expanded', true)
+        mNavBtnIcon.src = 'images/logo/Close.svg'
+      } else {
+        mobileNav.setAttribute('data-visible', false)
+        mNavBtn.setAttribute('aria-expanded', false)
+        mNavBtnIcon.src = 'images/logo/Menu.svg'
+      }
+    })
+})
+
 let items = document.querySelectorAll('.slider .list .item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
